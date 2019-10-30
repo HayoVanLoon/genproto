3e30be1576e1a7f62f805e4e2a0041f82d816685
Hayo van Loon <hayovanloon@gmail.com>
2019-10-30 18:25:06 +0100

    updates

--- update.sh
+++ update.sh
@@ -2,17 +2,17 @@
 
 if ! git add .; then
   echo "fail"
-  return 1
+  exit 1
 fi
 
 if ! git commit -m updates; then
   echo "fail"
-  return 1
+  exit 1
 fi
 
 if ! git push origin master; then
   echo "fail"
-  return 1
+  exit 1
 fi
 
 echo "done"
