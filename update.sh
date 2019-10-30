e371983951bf6ea17a9a8ab6af1a41461ca0d67f
Hayo van Loon <hayovanloon@gmail.com>
2019-10-30 18:34:47 +0100

    updates

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+OLD_DIR=$(pwd)
+cd ~/go/src/github.com/HayoVanLoon/genproto
+
 if ! git add .; then
   echo "fail"
   exit 1
@@ -14,5 +17,7 @@ if ! git push origin master; then
   echo "fail"
   exit 1
 fi
+
+cd "${OLD_DIR}"
 
 echo "done"
